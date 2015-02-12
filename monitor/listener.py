eeec9ac3c404d3b7706ada63978308bcedb01c17
lavi <zhaoximo@hotmail.com>
2015-02-12 11:32:49 +0800

    在81探针上测没问题

--- monitor/listener.py
+++ monitor/listener.py
@@ -34,60 +34,52 @@ class listener(DRH):
 
     #执行监听任务
     def handle(self):
+        print(100*"=")
         self.db = mydb(host='localhost',user="root",passwd='',port='3306')
         
-        #建socket
-        #sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-
-        #确保reuse
-        #sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-
-        #绑定端口
-        #sock.bind(server_address)
-
-        #接收的信息
-        #data,addr = sock.recvfrom(data_payload)
         data = self.request[0]
 
         #如果有数据
         if data:
-            print("data",data)
 
             #解端口
             ip,port = self.client_address
-            print("ip %s port %s"%(ip,port))
+            print("探针ip %s port %s"%(ip,port))
+            print("数据",data)
 
-            #step1 获取设备名
+            #step1 从设备基本信息表中获取设备名
             device_name = self.get_probe_name_from_ip_port(ip,port)
-            print("device_name",device_name)
 
             #step2 从探针信息表中获取syslog端口, 探针型号, 病毒日志统计, 监控网段
-            device_port,device_type,virus_statistic,network_segment = self.get_all_info_from_probe_info(device_name)
-            print("设备端口",device_port)
-            print("设备型号",device_type)
-            print("病毒统计",virus_statistic)
-            print("监控网段",network_segment)
+            all_info = self.get_all_info_from_probe_info(device_name)
+            if all_info == None:
+                logging.warning("can not find any information in probe info")
+                return None
+            device_port,device_type,virus_statistic,network_segment = all_info
 
             #step3 从探针型号信息表中得到全部信息
-            brand,coding_type,matching_rule,matching_position = self.get_all_from_probe_type_info(device_type)
-            print("品牌信息",brand)
-            print("编码格式",coding_type)
-            print("匹配规则",matching_rule)
-            print("映射字段",matching_position)
+            all_info = self.get_all_from_probe_type_info(device_type)
+            if all_info == None:
+                logging.warning("can not find any information in probe type info")
+                return None
+            brand,coding_type,matching_rule,matching_position = all_info
 
             #step4 将data转码, 转成utf-8
             data = str(data,encoding=coding_type)
             data = data.encode()
             data = str(data,encoding="utf-8")
 
-            print("data now is : ",data)
-
             #step5 用正则表达式抓取
             pattern = re.compile(matching_rule,re.DOTALL)
-            print("匹配规则|%s|"%(matching_rule))
-            temp_position = pattern.findall(data)[0]
-            print(temp_position)
+            temp_position = pattern.findall(data)
 
+            #如果正则表达式没有找出来, 说明不是病毒日志, 不进行存储 
+            if len(temp_position) == 0:
+                logging.warning("this is not a virus log")
+                return None
+            else:
+                temp_position = temp_position[0]
+            
 
             #step6 检验合法性=====未做
 
@@ -98,12 +90,8 @@ class listener(DRH):
             ndict["probe_ip"] = ip
             ndict["infected_time"] = get_time_str()
 
-            print(ndict)
-
             #step 8 存
             insert_str = probe_log_info.insert_str(ndict)
-            print("============================================")
-            print("插入语句",insert_str)
             self.db.execute_sql(insert_str,db_name)
                 
         else:
@@ -136,6 +124,7 @@ class listener(DRH):
             logging.error("more than one record has been found, error in device_info")
         elif len(result) == 0:
             logging.error("no record has been found, error in device_info")
+            return None
         else:
             pass
 
@@ -151,8 +140,10 @@ class listener(DRH):
         #如果结果大于1, 说明数据库录入有错误
         if len(result) > 1:
             logging.error("more than one record has been found, error in device_info")
+            return None
         elif len(result) == 0:
             logging.error("no record has been found, error in device_info")
+            return None
         else:
             pass
 
@@ -164,7 +155,7 @@ class listener(DRH):
         network_segment = result[4]
         
         #这里是采取硬结构进行编码, 因为返回的就是个list, 后面考虑修改    
-        return device_port,device_type,virus_statistic,network_segment
+        return [device_port,device_type,virus_statistic,network_segment]
 
     #sql3 从探针型号信息表中得到数据
     def get_all_from_probe_type_info(self,device_type):
